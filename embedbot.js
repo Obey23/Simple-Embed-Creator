1b157c2560a070ba5b4779a6f9d5355a6162002e
ObeyBeastJj23 <jarrodchriston6@gmail.com>
2019-06-12 03:19:20 +0100

    Initial Code
    Upload the good stuff.

---
+++ embedbot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client()
+var config = require("./config.json")
+const prefix = "/"
+
+bot.on("ready", () => {
+    bot.login(config.token);
+    console.log('----------------------------------------------------------')
+    console.log('[Obeys Embed Bot] Connected to Discord via the token successfully.')
+    console.log('[INFO] Username: ' + bot.user.username)
+	console.log('[INFO] Commands List: /embed')
+})
+
+bot.on("message", msg => {
+    if (msg.author.id != config.userid) { return; }
+    let cmd = msg.content.split(" ")[0]
+    cmd = cmd.slice(prefix.length)
+    let args = msg.content.split(" ").slice(1)
+    if (cmd === "embed" || cmd === "e") {
+		if(args[0].includes("#") === true) {
+			let message = args.slice(1).join(" ");
+			msg.edit("", { embed: new Discord.RichEmbed().setColor(args[0]).setDescription(message) });
+		}
+		else {
+			let message = args.join(" ");
+			msg.edit("", { embed: new Discord.RichEmbed().setDescription(message) });
+		}
+    }
+})
+
+bot.login(config.token);
---
+++ install-discord-js.bat
@@ -0,0 +1,3 @@
+@echo off
+
+npm install discord.js
